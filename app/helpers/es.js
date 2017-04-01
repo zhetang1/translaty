04ab19990d504897dd950d39f359af2a4427b487
tangzhe <tangzhe@amazon.com>
2017-03-31 23:21:04 -0400

    add a function to query aws es

---
+++ app/helpers/es.js
@@ -0,0 +1,30 @@
+import Elasticsearch from 'aws-es';
+
+const elasticsearch = new Elasticsearch({
+    accessKeyId: 'AKIAJ7CNS6RP6DJHZQMA',
+    secretAccessKey: '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',
+    service: 'es',
+    region: 'us-east-1',
+    host: 'search-translaty-sdt77wmwvyrqmzgawr4cviowhq.us-east-1.es.amazonaws.com'
+});
+
+export function search(keyword) {
+    return new Promise(function (resolve, reject) {
+        elasticsearch.search(
+            {
+                index: 'myindex',
+                type: 'mytype',
+                body: {
+                    query: {
+                        "match": {
+                            "text.S": keyword
+                        }
+                    }
+                }
+            },
+            function(err, data) {
+            if(err !== null) return reject(err);
+            resolve(data)
+        })
+    });
+}
